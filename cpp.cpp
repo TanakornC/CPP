f8af9f856486e5fe497f7a19cb0a3f6cf01b938d
TanakornC <139094147+TanakornC@users.noreply.github.com>
2023-07-11 18:43:04 +0700

    Add files via upload

---
+++ cpp.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " Student Code	: 66-06021620147" << endl;
+	cout << " Student Name	: Tanakorn Tongdee" << endl;
+	cout << "	Division: Information Technology" << endl;
+	cout << "	Year	: 1" << endl;
+	cout << "	Room	: RB" << endl;
+	cout << "	Sex	: Male" << endl;
+	cout << "	Age	: 18" << endl;
+
+}
+
+
+#include <iostream>
+using namespace std;
+int main() {
+
+			cout << " ™******"  << endl;
+			cout << "*" << endl;
+			cout << "*" << endl;   
+			cout << "*" << endl;
+			cout << "*" << endl;
+			cout << " ******"<<endl;
+
+	system("pause");
+
+	return 0;
+
+}
