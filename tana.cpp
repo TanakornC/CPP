5892c09936709dc5e4a46b1d51582393982d0a9e
TanakornC <139094147+TanakornC@users.noreply.github.com>
2023-07-17 11:55:06 +0700

    Add files via upload

--- tana.cpp
+++ tana.cpp
@@ -2,16 +2,23 @@
 #include<string>
 using namespace std;
 int main ()
-{	string Name;
-	float w;
-	float h ;
-	cout<<"Enter weight :";
-	cin>>w;
-	cout<<"Enter height :";
-	cin>>h;
-	cout<<"Output\n";
-	cout<<"BMI = " <<w/((h/100)*(h/100))<<endl;
+{	string name;
+	int salary;
+	int sale;
+	float com;
+	cout<<"Enter name : ";
+	cin>>name;
+	cout<<"Enter Salary : ";
+	cin>>salary;
+	cout<<"Enter Sale : ";
+	cin>>sale;
+	cout<<"Enter Commission :";
+	cin>>com;
+	cout<<"***OUTPUT***\n"<<endl;
+	cout<<"name"<<endl;
+	cout<<"Total Revenue "<<salary+(sale*(com/100))<<endl;
+
 
 	system("pause");
-	return (0);
+	return(0);
 }
