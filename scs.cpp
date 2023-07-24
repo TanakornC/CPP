26eef3f748ee9ea74c707e564b27b94cb471d173
TanakornC <139094147+TanakornC@users.noreply.github.com>
2023-07-24 10:06:49 +0700

    Add files via upload

---
+++ scs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float score
+	cout<<"Enter your score :";
+	cin>> score;
+	cout<< score<<"Test"<<((score>50)? "You Pass with score" : "You Pass with Fail")<<endl;
+	system ("pause")
+
+	return(0);
+}
+	
